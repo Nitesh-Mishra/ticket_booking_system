a4254a1d13ace4091162c49907211d992e1c8baf
nitesh-mishra <nitesh.mishra143@gmail.com>
2015-09-10 00:55:18 +0530

    added more data on pdf from schedule table

--- app/pdf/receipt_pdf.rb
+++ app/pdf/receipt_pdf.rb
@@ -2,6 +2,7 @@ class ReceiptPdf < Prawn::Document
   def initialize(booking, view)
     super(top_margin: 30)
     @booking = booking
+    @schedule = Schedule.find(@booking.schedule_id)
     @view = view
     booking_details
 
@@ -10,11 +11,16 @@ class ReceiptPdf < Prawn::Document
   def booking_details
     text "Booking Details",size: 25, style: :bold, align: :center 
     
-    text "Booked Id : \##{@booking.id}", top_margin: 10
+    text "Booked Id : \##{@booking.id}"
+    
+    text "Match between teams : #{@schedule.team_name1} Vs #{@schedule.team_name2}"  
+    text "Date : #{@schedule.date} "
+    text "Time : #{@schedule.time.strftime("%I:%M%p")} "
     text "No. of Seats Booked : #{@booking.no_of_seats_booked}"
-    text "Seat Type : : #{@booking.seat_type}"
+    text "Seat Type : #{@booking.seat_type}"
     text "Cost of Ticket : #{@booking.cost_of_ticket}"
     text "Payment Method : #{@booking.payment_method}"
+
   end
 
 end
